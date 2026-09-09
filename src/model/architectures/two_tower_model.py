d3c070abceb5608d927ead74e01755d5f2f35319
liad cojocarau <liad@Liad>
2026-09-09 16:26:11 +0300

    adding dirs

---
+++ src/model/architectures/two_tower_model.py
@@ -0,0 +1,129 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import date 
+from sklearn.base import BaseEstimator, RegressorMixin
+from src.model.architectures.two_tower import TwoTower
+
+
+class TwoTowerModel(BaseEstimator, RegressorMixin):
+
+    def __init__(
+        self,
+        user_features,
+        movie_features,
+        embedding_dim=64,
+        learning_rate=0.001,
+        epochs=10,
+        batch_size=1024
+    ):
+        self.user_features = user_features
+        self.movie_features = movie_features
+
+        self.embedding_dim = embedding_dim
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.batch_size = batch_size
+
+        self.model = TwoTower(
+            embedding_dim=embedding_dim
+        )
+
+    def _build_inputs(self, X):
+
+        # Get user features according to userId
+        user_input = (
+            self.user_features
+            .loc[X["userId"]]
+            .to_numpy(dtype=np.float32)
+        )
+
+        # Get movie features according to movieId
+        movie_input = (
+            self.movie_features
+            .loc[X["movieId"]]
+            .to_numpy(dtype=np.float32)
+        )
+
+        return {
+            "user": user_input,
+            "movie": movie_input
+        }
+
+    def fit(self, X, y):
+
+        self.model.compile(
+            optimizer=tf.keras.optimizers.Adam(
+                learning_rate=self.learning_rate
+            ),
+            loss="mse",
+            metrics=[
+                tf.keras.metrics.RootMeanSquaredError()
+            ]
+        )
+
+        inputs = self._build_inputs(X)
+
+        self.model.fit(
+            inputs,
+            np.asarray(y, dtype=np.float32),
+            epochs=self.epochs,
+            batch_size=self.batch_size,
+            validation_split=0.2,
+            verbose=1
+        )
+
+        return self
+
+    def predict(self, X):
+
+        inputs = self._build_inputs(X)
+
+        predictions = self.model(inputs, training=False).numpy()
+
+        return predictions.ravel()
+    
+    def find_recommendations(self, user_ids, num_of_recomands=20):
+        
+        user_input = (
+            self.user_features
+            .loc[user_ids]
+            .to_numpy(dtype=np.float32)
+        )
+
+
+        user_embeddings = self.model.user_tower(
+            user_input
+        )
+
+        movie_embeddings = self.model.movie_tower(
+            self.movie_features.to_numpy(dtype=np.float32)
+        )
+
+        scores = tf.matmul(
+            user_embeddings,
+            movie_embeddings,
+            transpose_b=True
+        )
+
+        top_movies = tf.math.top_k(
+            scores,
+            k=num_of_recomands
+        )
+
+        recommendations = []
+
+        for i, user_id in enumerate(user_ids):
+
+            movie_indices = top_movies.indices[i].numpy()
+
+            movie_ids = self.movie_features.index[
+                movie_indices
+            ]
+
+            recommendations.append(
+                (user_id, movie_ids)
+            )
+
+        return recommendations
+
---
+++ src/model/evaluation/benchmark.py
@@ -0,0 +1,182 @@
+import joblib
+from src.model.architectures.two_tower_model import TwoTowerModel
+import time
+import pandas as pd
+import os
+import numpy as np
+
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+import kagglehub
+
+PATH = kagglehub.dataset_download("grouplens/movielens-20m-dataset")
+
+
+def read_movies(movies):
+    mlb = MultiLabelBinarizer()
+    genre_features = mlb.fit_transform(movies['genres'].str.split('|'))
+    genre_df = pd.DataFrame(
+        genre_features,
+        columns=mlb.classes_,
+        index=movies["movieId"]
+    )
+    return genre_df
+
+
+def build_user_genre_profile_matrix(movies, ratings):
+    """
+    building a user-genre profile matrix where each row corresponds to 
+    a user and each column corresponds to a genre. 
+    The values in the matrix represent the average rating given 
+    by the user to movies of that genre.
+    """
+    
+    user_genre = ratings.merge(
+        movies,
+        left_on="movieId",
+        right_index=True
+    )
+    
+    genre_columns = movies.columns.to_list()
+    user_genre_melted = user_genre.melt(
+        id_vars=["userId", "rating"], 
+        value_vars=genre_columns, 
+        var_name="genre", 
+        value_name="present"
+    )
+    user_genre_melted = user_genre_melted[user_genre_melted["present"] == 1]
+
+    user_genre_matrix = user_genre_melted.groupby(["userId", "genre"])["rating"].mean().unstack()
+
+    genre_means = user_genre_matrix.mean()
+    user_genre_matrix = user_genre_matrix.fillna(genre_means)
+    
+    return user_genre_matrix
+
+
+def learning():
+    print("Loading data...")
+    movies_raw = pd.read_csv(os.path.join(PATH, "movie.csv"))
+    movies = read_movies(movies_raw)
+    ratings = pd.read_csv(os.path.join(PATH, "rating.csv"), nrows=500_000)
+
+    print("Building user feature matrix...")
+    users_features = build_user_genre_profile_matrix(movies, ratings)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        ratings[["userId", "movieId"]],
+        ratings["rating"],
+        test_size=0.2,
+        random_state=42
+    )
+
+    embedding_dim = 32
+    batch_size = 4 * 1024
+    epochs = 7
+    learning_rate = 3e-3
+
+    print("Training Two-Tower Model...")
+    model = TwoTowerModel(
+        user_features=users_features,
+        movie_features=movies,
+        epochs=epochs,
+        batch_size=batch_size,
+        embedding_dim=embedding_dim,
+        learning_rate=learning_rate
+    )
+    model.fit(X_train, y_train)
+    testing(model, X_train, y_train, X_test, y_test)
+    return model, users_features.index.to_numpy()
+
+def testing(model, X_train,y_train, X_test, y_test):
+    print("Evaluating model on test set...")
+    start_train_time = time.time()
+    train_predictions = model.predict(X_train)
+    end_train_time = time.time()
+    train_duration = end_train_time - start_train_time
+    print(f"Train prediction time: {train_duration:.2f} seconds")
+    start_test_time = time.time()
+    test_predictions = model.predict(X_test)
+    end_test_time = time.time()
+    test_duration = end_test_time - start_test_time
+    print(f"Test prediction time: {test_duration:.2f} seconds")
+    
+    train_rmse = np.sqrt(np.mean((train_predictions - y_train) ** 2))
+    test_rmse = np.sqrt(np.mean((test_predictions - y_test) ** 2))
+
+    print(f"Train RMSE: {train_rmse:.4f}")
+    print(f"Test RMSE: {test_rmse:.4f}")
+
+
+def benchmark_throughput(model, available_user_ids, batch_size=64, num_batches=200):
+    """
+    מבצעת בדיקת עומס (Stress Test) ומחשבת Throughput ו-Latency Percentiles.
+    """
+    print("\n===========================================================================")
+    print(f"Starting Benchmark: {num_batches} requests | Batch size: {batch_size} users/request")
+    print("===========================================================================\n")
+
+    latencies_ms = []
+    total_users_processed = 0
+
+    dummy_users = np.random.choice(available_user_ids, size=batch_size, replace=True)
+    _ = model.find_recommendations(dummy_users)
+
+    global_start_time = time.perf_counter()
+
+    for _ in range(num_batches):
+        # 1. sample users
+        batch_users = np.random.choice(available_user_ids, size=batch_size, replace=True)
+
+        # 2. time the request
+        req_start = time.perf_counter()
+        _ = model.find_recommendations(batch_users)
+        req_end = time.perf_counter()
+
+        latency = (req_end - req_start) * 1000  # convert to ms
+        latencies_ms.append(latency)
+        total_users_processed += batch_size
+
+    global_end_time = time.perf_counter()
+    total_duration_sec = global_end_time - global_start_time
+
+    # compute throughput metrics
+    rps = num_batches / total_duration_sec
+    users_per_sec = total_users_processed / total_duration_sec
+
+    # compute latency percentiles
+    p50 = np.percentile(latencies_ms, 50)
+    p95 = np.percentile(latencies_ms, 95)
+    p99 = np.percentile(latencies_ms, 99)
+    avg_lat = np.mean(latencies_ms)
+
+    # print the results
+    print("📊 PERFORMANCES REPORT:")
+    print(f"  • Total Time Elapsed     : {total_duration_sec:.2f} seconds")
+    print(f"  • Total Requests Processed: {num_batches}")
+    print(f"  • Total Users Served      : {total_users_processed:,}")
+    print(f"--------------------------------------------------")
+    print(f"🚀 THROUGHPUT:")
+    print(f"  • Requests Per Second (RPS): {rps:.2f} req/sec")
+    print(f"  • Users Per Second (UPS)   : {users_per_sec:.2f} users/sec")
+    print(f"--------------------------------------------------")
+    print(f"⏱️ LATENCY DISTRIBUTION:")
+    print(f"  • Average Latency : {avg_lat:.2f} ms")
+    print(f"  • P50 (Median)    : {p50:.2f} ms")
+    print(f"  • P95             : {p95:.2f} ms")
+    print(f"  • P99 (Tail)      : {p99:.2f} ms")
+    print("===========================================================================")
+
+
+def main():
+    model, available_user_ids = learning()
+    batch_sizes = [32,64,128,256]
+    for batch_size in batch_sizes:
+        print(f"\n\nBenchmarking with batch size: {batch_size}")
+        print("--------------------------------------------------")
+        benchmark_throughput(model, available_user_ids, batch_size=batch_size, num_batches=200)
+    
+
+
+if __name__ == "__main__":
+    main()
